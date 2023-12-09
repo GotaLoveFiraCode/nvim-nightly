375745c3cdcac67d03082c9a52971a85edacce62
Laurens Rosinski <rosinski@duck.com>
2023-12-09 20:59:13 +0100

    fix(cybu): customized cybu

--- lua/plugins/cybu.lua
+++ lua/plugins/cybu.lua
@@ -11,7 +11,36 @@ return {
 			print('"cybu" failed to load')
 			return
 		end
-		cybu.setup()
+
+		cybu.setup {
+			position = {
+				anchor = "bottomleft",
+				-- topleft, topcenter, topright,
+				-- centerleft, center, centerright,
+				-- bottomleft, bottomcenter, bottomright
+			},
+
+			style = {
+				path = "relative",            -- absolute, relative, tail (filename only)
+				border = "rounded",           -- single, double, rounded, none
+				separator = " ",              -- string used as separator
+				prefix = "…",                 -- string used as prefix for truncated paths
+				padding = 1,                  -- left & right padding in number of spaces
+
+				devicons = {
+					enabled = true,             -- enable or disable web dev icons
+					colored = true,             -- enable color for web dev icons
+					truncate = true,            -- truncate wide icons to one char width
+				},
+
+				highlights = {                -- see highlights via :highlight
+					current_buffer = "CybuFocus",       -- current / selected buffer
+					adjacent_buffers = "CybuAdjacent",  -- buffers not in focus
+					background = "CybuBackground",      -- window background
+					border = "CybuBorder",              -- border of the window
+				},
+			},
+		}
 	end,
 
 	keys = {
