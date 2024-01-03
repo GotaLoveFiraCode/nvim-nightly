bcc9961e841070793bf61b4efc6e4c966f9d2e32
Laurens Rosinski <rosinski@duck.com>
2024-01-03 19:40:58 +0100

    feat(removed ts-context): I don’t like it. Also changed treesitter to start at 'VeryLazy,' meaning it starts faster when entering a buffer now. I.e. treesitter improvements.

--- lua/plugins/ts_context.lua
+++ lua/plugins/ts_context.lua
@@ -1,5 +1,6 @@
 return {
 	"nvim-treesitter/nvim-treesitter-context",
+	enabled = false,
 	event = { 'BufRead', 'BufNewFile', 'BufWritePost' },
 	keys = {
 		{ '[c', function() require'treesitter-context'.go_to_context() end, silent = true, desc = "Go to context" }
