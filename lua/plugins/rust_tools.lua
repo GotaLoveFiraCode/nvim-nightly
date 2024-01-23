e5c4dee52a37e9d302869ed8d4352ab09ae2405e
Laurens Rosinski <rosinski@duck.com>
2024-01-23 13:19:08 +0100

    feat(rust,lsp): Added clippy linting to rust-analyzer.
    Added (pedantic) clippy linting to cargo projects. Clippy is invoked on every save. Clippy is called with `--no-deps` and `-Wclippy::pedantic` flags.

--- lua/plugins/rust_tools.lua
+++ lua/plugins/rust_tools.lua
@@ -12,6 +12,8 @@ return {
 		local rt = require'rust-tools'
 
 		rt.setup { -- {{{
+			tools = { inlay_hints = { only_current_line = true } },
+
 			server = {
 				capabilities = require'cmp_nvim_lsp'.default_capabilities(),
 				on_attach = function(_, bufnr)
@@ -19,6 +21,38 @@ return {
 					vim.keymap.set("n", "<leader>ca", rt.code_action_group.code_action_group, { buffer = bufnr })
 					vim.keymap.set("n", "<leader>R", rt.runnables.runnables, { buffer = bufnr })
 				end,
+				settings = {
+					["rust-analyzer"] = {
+
+						-- cargo = {
+						-- 	allFeatures = true,
+						-- 	loadOutDirsFromCheck = true,
+						-- 	runBuildScripts = true,
+						-- },
+
+						checkOnSave = {
+							-- allFeatures = true,
+							command = 'clippy',
+
+							extraArgs = {
+								'--',
+								'--no-deps',
+								'-Wclippy::pedantic',
+							},
+
+						},
+
+						-- procMacro = {
+						-- 	enable = true,
+						-- 	ignored = {
+						-- 		["async-trait"] = { "async_trait" },
+						-- 		["napi-derive"] = { "napi" },
+						-- 		["async-recursion"] = { "async_recursion" },
+						-- 	},
+						-- },
+
+					},
+				},
 			},
 		} -- }}}
 
