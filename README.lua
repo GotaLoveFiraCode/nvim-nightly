3b9c4b018cc3a0b03b01b590d86a5b37b477c81b
Laurens Rosinski <rosinski@duck.com>
2024-01-23 14:57:58 +0100

    doc(README): added README.

---
+++ README.lua
@@ -0,0 +1,4 @@
+# Neovim nightly config for the minimalist who forgot what that word means.
+
+
+It is much to much work for someone viewing a git repo to look at *all* the information about the project. Instead, I suggest you simply blindly trust me, and take my word for this neovim config being fantastic.
